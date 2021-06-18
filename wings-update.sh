f7113d9ed3f7254ea28bca5c6f4d094c38ee9946
PrestonJude <69034430+PrestonJude@users.noreply.github.com>
2021-06-18 13:45:19 -0700

    Update wings-update.sh

--- wings-update.sh
+++ wings-update.sh
@@ -1,1 +1,11 @@
-
+echo -e "\e[1;31m Your Pterodactyl Wings Are Updating \e[0m"
+sleep 5
+cd /usr/local/bin
+sleep 1
+curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
+chmod u+x /usr/local/bin/wings
+sleep 1
+systemctl restart wings
+sleep 1
+clear
+echo -e "\e[1;31m Your Pterodactyl Wings Have Completley Updated! \e[0m"
